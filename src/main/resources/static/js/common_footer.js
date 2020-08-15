da291a72c687b67029d7670992338dcd058e669c
zhanghuan <18672766561@126.com>
2020-08-15 18:56:51 +0800

    SpringBoot版本更换2.0.1，编写SpringBootTest类，insert语句控制事务回滚

---
+++ src/main/resources/static/js/common_footer.js
@@ -0,0 +1,3 @@
+document.write('<script type="text/javascript" src="/static/js/jquery-2.0.0.min.js"></script>');
+document.write('<script type="text/javascript" src="/static/js/bootstrap.min.js"></script>');
+document.write('<script type="text/javascript" src="/static/js/jquery-ui.js"></script>');
---
+++ src/main/resources/static/js/common_head.js
@@ -0,0 +1,2 @@
+document.write('<link href="/static/css/bootstrap-combined.min.css" rel="stylesheet" />');
+document.write('<link href="/static/css/layoutit.css" rel="stylesheet" />');
