93575fd45f81f65226a48ceac113880a8ddd7838
zhanghuan <18672766561@126.com>
2020-08-29 14:33:57 +0800

    修改页面访问controller的名称
    Signed-off-by: zhanghuan <18672766561@126.com>

--- src/main/java/com/didispace/mybatis/employees/service/EmployeesService.java
+++
@@ -1,9 +0,0 @@
-package com.didispace.mybatis.employees.service;
-
-import java.util.List;
-
-import com.didispace.mybatis.employees.bean.Employees;
-
-public interface EmployeesService {
-	 public List<Employees> selectById(String employeeID);
-}
--- src/main/java/com/didispace/thymeleaf/WebProjectController.java
+++ src/main/java/com/didispace/thymeleaf/WebProjectController.java
@@ -12,81 +12,9 @@ public class WebProjectController {
 	
 	Logger logger = LoggerFactory.getLogger(WebProjectController.class);
 	
-	@RequestMapping(value = "/index",method = RequestMethod.GET)
-    public ModelAndView index() {
-        ModelAndView mv = new ModelAndView();
-        mv.addObject("name", "许三多");
-        mv.addObject("hasLogon","false");
-        mv.setViewName("/thymeleaf/index");
-        return mv;
-    }
-	
-	@RequestMapping(value = "/case_",method = RequestMethod.GET)
-    public ModelAndView case_() {
-        ModelAndView mv = new ModelAndView();
-        mv.addObject("name", "许三多");
-        mv.addObject("hasLogon","false");
-        mv.setViewName("/thymeleaf/case_");
-        return mv;
-    }
-	
-	@RequestMapping(value = "/about",method = RequestMethod.GET)
-	public ModelAndView about() {
-		ModelAndView mv = new ModelAndView();
-		mv.addObject("name", "许三多");
-		mv.addObject("hasLogon","false");
-		mv.setViewName("/thymeleaf/about");
-		return mv;
-	}
-	
-	@RequestMapping(value = "/information",method = RequestMethod.GET)
-	public ModelAndView information() {
-		ModelAndView mv = new ModelAndView();
-		mv.addObject("name", "许三多");
-		mv.addObject("hasLogon","false");
-		mv.setViewName("/thymeleaf/information");
-		return mv;
-	}
-	
-	@RequestMapping(value = "/register",method = RequestMethod.GET)
-	public ModelAndView register() {
-		ModelAndView mv = new ModelAndView();
-		mv.addObject("name", "许三多");
-		mv.addObject("hasLogon","false");
-		mv.setViewName("/thymeleaf/register");
-		return mv;
-	}
-	
-	/**
-	 * 接收页面的参数，只需  变量名    与页面  name  的名称保持一致即可
-	 * @param username
-	 * @param userpassword
-	 * @param useremail
-	 * @return
-	 */
-	@RequestMapping(value = "/newUser",method = RequestMethod.POST)
-	public ModelAndView newUser(String username,String userpassword,String useremail) {
-		logger.info("username" +"————>"+ username);
-		logger.info("userpassword"+"————>"+ userpassword);
-		logger.info("useremail"+"————>"+ useremail);
-		
-		ModelAndView mv = new ModelAndView();
-		mv.addObject("retMsg", "注册成功!");
-		mv.addObject("hasLogon","true");
-		mv.addObject("username","用户名:"+username);
-		mv.addObject("useremail","注册使用邮件:" +useremail);
-		
-		mv.setViewName("/thymeleaf/newUser");
-		return mv;
-	}
-	
-	@RequestMapping(value = "/employees",method = RequestMethod.GET)
-	public ModelAndView employees() {
-		ModelAndView mv =  new ModelAndView();
-		mv.addObject("hasLogon","true");
-		mv.setViewName("/thymeleaf/employees");
-		return mv;
-	}
+
+
+
 	
 
 	
